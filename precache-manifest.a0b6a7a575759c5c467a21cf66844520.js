6c0458955df888f4b39480445766705597942d2a
Yuvraj Kaushal <ykaushal364@gmail.com>
2021-06-11 03:36:38 +0530

    Updates

---
+++ precache-manifest.a0b6a7a575759c5c467a21cf66844520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f562187e8f1f50756f1c66a72f895d7a",
+    "url": "/6th_ReactJS/index.html"
+  },
+  {
+    "revision": "a2bef90c92c7c499b8ec",
+    "url": "/6th_ReactJS/static/js/2.c245fa8e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/6th_ReactJS/static/js/2.c245fa8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15acffce0398ef94ed5a",
+    "url": "/6th_ReactJS/static/js/main.bec6ee8e.chunk.js"
+  },
+  {
+    "revision": "8deb806f605e96ee7608",
+    "url": "/6th_ReactJS/static/js/runtime-main.a9b87093.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/6th_ReactJS/precache-manifest.a0b6a7a575759c5c467a21cf66844520.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/6th_ReactJS/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
